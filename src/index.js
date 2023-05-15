4a5bd8f9d011d29da86d984a9133ccb525f9dd25
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-05-15 15:52:51 +0300

    refs and EventListener

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@ import './css/styles.css';
 
 // Import as a Module
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  inputSearchBoxEl: document.getElementById('search-box'),
+  countryListEl: document.getElementsByClassName('country-list'),
+  countryInfoListEl: document.getElementsByClassName('country-info'),
+};
+
+const searchValue = refs.inputSearchBoxEl.value;
+
+// console.dir(searchValue);
+// console.dir(refs.countryListEl);
+// console.log(refs.countryInfoListEl);
+
+refs.inputSearchBoxEl.addEventListener('input', onInputValue);
+
+function onInputValue(searchValue) {
+  e.preventDefault();
+}
+
+// function onCreateCountryList(params) {}
+
+// function onCreateCountryInfoList(params) {}
